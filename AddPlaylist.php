ad6bae99c397ec013b945f84120b2b4d30ea0cee
jjchan1 <jjchan1@gmail.com>
2017-12-12 16:27:47 -0500

    Deprecated

--- AddPlaylist.php
+++
@@ -1,9 +0,0 @@
-<?php
-require("methods.php");
-$db = connect();
-if ($_GET["playlistName"]){
-    createPlaylist($db, $_GET["playlistName"]);
-}
-header("Location: EditPlaylist.php?playlistName=".$_GET["playlistName"]);
-$db->close();
-?>
